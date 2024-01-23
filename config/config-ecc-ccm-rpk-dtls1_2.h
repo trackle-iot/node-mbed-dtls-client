7b765e7e34f8f4c50c563890af5d2d6784bc720f
Andrea Taffi <andrea.taffi@gmail.com>
2024-01-23 12:25:02 +0100

    fix: max content length

--- config/config-ecc-ccm-rpk-dtls1_2.h
+++ config/config-ecc-ccm-rpk-dtls1_2.h
@@ -93,7 +93,7 @@
  * both ends of the connection!  (See comments in "mbedtls/ssl.h".)
  * The optimal size here depends on the typical size of records.
  */
-#define MBEDTLS_SSL_MAX_CONTENT_LEN             768
+#define MBEDTLS_SSL_MAX_CONTENT_LEN             1400
 
 #include "mbedtls/check_config.h"
 
